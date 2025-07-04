e4e10cf08648cb053e1ddad0fe232db539d066b2
MoazamAli45 <smoazam987@gmail.com>
2025-07-04 23:23:24 +0500

    Update settings.py to enhance security and static file handling

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -74,9 +74,9 @@ LOGGING = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost','socialmedia-dun.vercel.app']
 
 
 # Application definition
@@ -203,8 +203,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Where hero.png is located
+STATIC_ROOT = BASE_DIR / 'staticfiles'    # For collectstatic
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
